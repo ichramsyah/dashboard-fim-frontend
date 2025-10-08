417b0a3611cf4879271b6c382b11c47c8ca55ddb
ichramsyah <iamabdulrahman15@gmail.com>
2025-10-08 09:33:50 +0700

    testing

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value;
   const { pathname } = request.nextUrl;
-  const protectedPaths = ['/log', '/trash'];
+  const protectedPaths = ['/logoo', '/trashoo'];
 
   if (!token && protectedPaths.includes(pathname)) {
     return NextResponse.redirect(new URL('/login', request.url));
