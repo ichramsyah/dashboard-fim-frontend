a5de7d13f6c638cc997d3e2cfbc5b7626908f4ba
ichramsyah <iamabdulrahman15@gmail.com>
2025-10-15 11:23:47 +0700

    update homepage

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -55,11 +55,11 @@ export default function Home() {
   }, [days]);
 
   const StatCard = ({ icon, title, value, color, bgColor }: any) => (
-    <div className={`flex-1 px-4 py-3 rounded-lg flex items-center ${bgColor}`}>
-      <div className={`p-3 rounded-full mr-1 ${color} bg-white`}>{icon}</div>
+    <div className={`flex-1 py-3 rounded-lg flex justify-start md:justify-center items-center ${bgColor}`}>
+      <div className={` rounded-full mr-4 ${color} bg-white`}>{icon}</div>
       <div>
-        <p className="text-sm text-gray-600">{title}</p>
-        <p className="text-2xl font-bold text-gray-800">{value}</p>
+        <p className="text-sm text-gray-700">{title}</p>
+        <p className="text-2xl font-bold text-gray-600">{value}</p>
       </div>
     </div>
   );
@@ -113,12 +113,13 @@ export default function Home() {
             {/* Bagian Statistik Hari Ini */}
             <div className="mt-8 px-1 w-full pb-6">
               <h2 className="text-gray-7 text-lg font-semibold mb-3 flex items-center gap-2">Hari Ini</h2>
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mt-6 gap-4">
-                <StatCard icon={<FaShieldAlt size={22} />} title="Total Perubahan" value={todayStats?.total_perubahan_hari_ini ?? 0} color="text-gray-700" bgColor="" />
-                <StatCard icon={<FaBell size={22} />} title="Normal" value={todayStats?.detail.normal ?? 0} color="text-blue-500" bgColor="bg-whitee" />
-                <StatCard icon={<FaExclamationTriangle size={22} />} title="Mencurigakan" value={todayStats?.detail.mencurigakan ?? 0} color="text-yellow-500" bgColor="bg-whitee" />
-                <StatCard icon={<FaExclamationTriangle size={22} />} title="Bahaya" value={todayStats?.detail.bahaya ?? 0} color="text-red-500" bgColor="bg-whitee" />
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mt-3">
+                <StatCard icon={<FaShieldAlt size={22} />} title="Total Perubahan" value={todayStats?.total_perubahan_hari_ini ?? 0} color="text-gray-700/80" bgColor="" />
+                <StatCard icon={<FaBell size={22} />} title="Normal" value={todayStats?.detail.normal ?? 0} color="text-blue-500/80" bgColor="bg-whitee" />
+                <StatCard icon={<FaExclamationTriangle size={22} />} title="Mencurigakan" value={todayStats?.detail.mencurigakan ?? 0} color="text-yellow-500/80" bgColor="bg-whitee" />
+                <StatCard icon={<FaExclamationTriangle size={22} />} title="Bahaya" value={todayStats?.detail.bahaya ?? 0} color="text-red-500/80" bgColor="bg-whitee" />
               </div>
+              {/* Disini */}
             </div>
           </div>
         )}
