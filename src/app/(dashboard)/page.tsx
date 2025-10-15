9dda57c82752d9d79f27fa4cde6286da8307629e
ichramsyah <iamabdulrahman15@gmail.com>
2025-10-15 11:00:12 +0700

    update home page

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -55,8 +55,8 @@ export default function Home() {
   }, [days]);
 
   const StatCard = ({ icon, title, value, color, bgColor }: any) => (
-    <div className={`flex-1 p-4 rounded-lg flex items-center ${bgColor}`}>
-      <div className={`p-3 rounded-full mr-4 ${color} bg-white`}>{icon}</div>
+    <div className={`flex-1 px-4 py-3 rounded-lg flex items-center ${bgColor}`}>
+      <div className={`p-3 rounded-full mr-1 ${color} bg-white`}>{icon}</div>
       <div>
         <p className="text-sm text-gray-600">{title}</p>
         <p className="text-2xl font-bold text-gray-800">{value}</p>
@@ -66,7 +66,7 @@ export default function Home() {
 
   return (
     <main className="min-h-screen">
-      <div className="container mx-auto max-w-7xl px-4">
+      <div className="container mx-auto max-w-7xl md:px-4 px-1">
         <h1 className="text-2xl font-bold mb-4">Analisis Aktivitas</h1>
 
         {error && <p className="text-center text-red-600 bg-red-100 p-4 rounded-md mb-4">{error}</p>}
@@ -99,9 +99,9 @@ export default function Home() {
                       <YAxis allowDecimals={false} fontSize={12} />
                       <Tooltip />
                       <Legend />
-                      <Line type="monotone" dataKey="detail.bahaya" name="Bahaya" stroke="#f06e6eff" strokeWidth={2} />
-                      <Line type="monotone" dataKey="detail.mencurigakan" name="Mencurigakan" stroke="#f3be64ff" strokeWidth={2} />
-                      <Line type="monotone" dataKey="detail.normal" name="Normal" stroke="#84aff5ff" strokeWidth={2} />
+                      <Line type="monotone" dataKey="detail.bahaya" name="Bahaya" stroke="#f48c8cff" strokeWidth={2} />
+                      <Line type="monotone" dataKey="detail.mencurigakan" name="Mencurigakan" stroke="#f0ce95ff" strokeWidth={2} />
+                      <Line type="monotone" dataKey="detail.normal" name="Normal" stroke="#a3c3f8ff" strokeWidth={2} />
                     </LineChart>
                   </ResponsiveContainer>
                 </div>
@@ -111,16 +111,13 @@ export default function Home() {
             </div>
 
             {/* Bagian Statistik Hari Ini */}
-
-            <div className="mt-8 px-1 w-full">
+            <div className="mt-8 px-1 w-full pb-6">
               <h2 className="text-gray-7 text-lg font-semibold mb-3 flex items-center gap-2">Hari Ini</h2>
-              <div className="md:w-1/2 lg:w-1/2 sm:w-full">
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4">
-                  <StatCard icon={<FaShieldAlt size={22} />} title="Total Perubahan" value={todayStats?.total_perubahan_hari_ini ?? 0} color="text-gray-700" bgColor="bg-gray-100" />
-                  <StatCard icon={<FaBell size={22} />} title="Normal" value={todayStats?.detail.normal ?? 0} color="text-blue-500" bgColor="bg-blue-50" />
-                  <StatCard icon={<FaExclamationTriangle size={22} />} title="Mencurigakan" value={todayStats?.detail.mencurigakan ?? 0} color="text-yellow-500" bgColor="bg-yellow-50" />
-                  <StatCard icon={<FaExclamationTriangle size={22} />} title="Bahaya" value={todayStats?.detail.bahaya ?? 0} color="text-red-500" bgColor="bg-red-50" />
-                </div>
+              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mt-6 gap-4">
+                <StatCard icon={<FaShieldAlt size={22} />} title="Total Perubahan" value={todayStats?.total_perubahan_hari_ini ?? 0} color="text-gray-700" bgColor="" />
+                <StatCard icon={<FaBell size={22} />} title="Normal" value={todayStats?.detail.normal ?? 0} color="text-blue-500" bgColor="bg-whitee" />
+                <StatCard icon={<FaExclamationTriangle size={22} />} title="Mencurigakan" value={todayStats?.detail.mencurigakan ?? 0} color="text-yellow-500" bgColor="bg-whitee" />
+                <StatCard icon={<FaExclamationTriangle size={22} />} title="Bahaya" value={todayStats?.detail.bahaya ?? 0} color="text-red-500" bgColor="bg-whitee" />
               </div>
             </div>
           </div>
