e880ef4ef11103786db5bdf82ba74eb1699a0f16
ichramsyah <iamabdulrahman15@gmail.com>
2025-11-03 11:13:16 +0700

    update navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -56,7 +56,7 @@ export default function Navbar({ toggleSidebar, isSidebarOpen, isMobile }: Navba
       className={`
         sticky top-0 z-20 
         transition-colors duration-300
-        ${isSidebarOpen && isMobile ? 'opacity-80' : 'bg-whitee'}
+        ${isSidebarOpen && isMobile ? 'opacity-80' : 'bg-background-dark'}
       `}
     >
       <div className="max-w-7xl mx-auto md:px-4 px-2 sm:px-6 lg:px-8">
@@ -67,24 +67,24 @@ export default function Navbar({ toggleSidebar, isSidebarOpen, isMobile }: Navba
 
           <div className="flex-1 flex items-center md:justify-between justify-end space-x-4 md:space-x-6">
             <div className="hidden sm:flex items-center text-[13px]">
-              <p className="text-gray-800 bg-gray-100 px-3 py-1.5 rounded-md space-x-2 ">{format(currentTime, 'EEEE, dd/MM/yyyy', { locale: id })}</p>
-              <p className="pl-3 text-gray-6 text-[13px]"> {format(currentTime, 'HH:mm:ss', { locale: id })} WIB</p>
+              <p className="text-gray-3 border border-gray-3 px-3 py-1.5 rounded-md space-x-2 ">{format(currentTime, 'EEEE, dd/MM/yyyy', { locale: id })}</p>
+              <p className="pl-3 text-gray-4 text-[13px]"> {format(currentTime, 'HH:mm:ss', { locale: id })} WIB</p>
             </div>
             <div className="flex items-center justify-between space-x-3 md:space-x-4">
               <div className="flex pr-2">
                 {incronStatus === null ? (
                   <p className="text-gray-500">Mengecek...</p>
                 ) : incronStatus ? (
-                  <div className="flex items-center">
-                    <h2 className="font-bold text-gray-700 pr-2">Incron Aktif</h2>
+                  <div className="flex items-center bg-gray-6/30 px-5 py-1.5 rounded-full">
+                    <h2 className="text-gray-300 pr-2 text-sm">Incron Aktif</h2>
                     <span className="relative flex h-3 w-3">
                       <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
-                      <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+                      <span className="relative inline-flex rounded-full h-3 w-3 bg-green-400"></span>
                     </span>
                   </div>
                 ) : (
-                  <div className="flex items-center">
-                    <h2 className="font-bold text-gray-700 pr-2">Incron Tidak Aktif</h2>
+                  <div className="flex items-center bg-gray-6/30 px-5 py-1.5 rounded-full">
+                    <h2 className="text-gray-300 pr-2 text-sm">Incron Tidak Aktif</h2>
                     <span className="relative flex h-3 w-3">
                       <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
                       <span className="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
@@ -93,7 +93,10 @@ export default function Navbar({ toggleSidebar, isSidebarOpen, isMobile }: Navba
                 )}
               </div>
 
-              <button onClick={handleLogout} className="bg-red-600 cursor-pointer text-white text-sm  px-3.5 py-1.5 rounded-[7px] hover:rounded-[100px] transition-all flex items-center space-x-1 duration-300">
+              <button
+                onClick={handleLogout}
+                className="border border-gray-3 cursor-pointer text-white text-sm px-3.5 py-1.5 rounded-[5px] hover:rounded-[100px] hover:bg-red-500 hover:border-red-500 transition-all flex items-center space-x-1 duration-300"
+              >
                 <FiLogOut /> <span>Logout</span>
               </button>
             </div>
